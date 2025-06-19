51685df363367e00f3b1f4f046f25686e8fa8f7d
Jordan Powell <jordan@jpdesigning.com>
2025-06-19 14:04:40 -0400

    fix typechecking

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import { Navigation } from './components/Navigation';
