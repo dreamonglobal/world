2d03e1128031b3e783f50d931b22dc3a4e783f2b
Jordan <jordan@jpdesigning.com>
2026-02-06 19:15:09 -0500

    Update Pakistan funding: +$1,000 raised (99% funded, only $400 to go!) (#24)

--- src/pages/Missions.tsx
+++ src/pages/Missions.tsx
@@ -18,7 +18,7 @@ const upcomingTrips = [
     cta: 'Support This Trip',
     ctaLink: '/donate',
     blogLink: '/blog/pakistan-2026-answering-the-call',
-    fundingCurrent: 38100,
+    fundingCurrent: 39100,
     fundingGoal: 39500,
   },
   {
